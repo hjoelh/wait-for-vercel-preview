d7325dfe39c888245bb06e5eaea1bf9c1d6af4ca
Joel <68335961+hjoelh@users.noreply.github.com>
2025-11-04 09:39:54 +0000

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -238,6 +238,8 @@ const waitForDeploymentToStart = async ({
           environment: env,
         });
 
+        console.log("all deployments", deployments)
+
         const deployment =
           deployments.data.length > 0 &&
           deployments.data.find((deployment) => {
